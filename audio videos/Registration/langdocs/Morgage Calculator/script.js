bda5a0a4c935606b604576607212db4e34bc080b
nereu-s <nereu.s@stu.janesville.k12.wi.us>
2025-04-24 14:33:23 -0500

    4/24/25

---
+++ audio videos/Registration/langdocs/Morgage Calculator/script.js
@@ -0,0 +1,9 @@
+do Math () {
+    let NumP= Number(document.getElementById("NumP").value);
+    let NumI= Number(document.getElementById("NumI").value);
+
+    let NumR=Number(document.getElementById("NumR").value);
+    
+    let B= NumP()
+}
+
