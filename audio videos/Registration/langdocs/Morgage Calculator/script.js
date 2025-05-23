a6e3a4c4db039430b336165008d8f96adc416e56
nereu-s <nereu.s@stu.janesville.k12.wi.us>
2025-05-23 14:27:40 -0500

    5.23

--- audio videos/Registration/langdocs/Morgage Calculator/script.js
+++ audio videos/Registration/langdocs/Morgage Calculator/script.js
@@ -1,9 +1,11 @@
-do Math () {
-    let NumP= Number(document.getElementById("NumP").value);
-    let NumI= Number(document.getElementById("NumI").value);
+function Math () {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let num3 = Number(document.getElementById("num3").value);
+    let num4 = Number(document.getElementById("num4").value);
+    let num5 = Number(document.getElementById("num5").value);
+    let num6 = Number(document.getElementById("12").value);
 
-    let NumR=Number(document.getElementById("NumR").value);
-    
-    let B= NumP()
+    let p = n1 * 
 }
 
---
+++ js/css/script.js
@@ -0,0 +1,21 @@
+functions () {
+    let Options = (document.getElementById("new").value);
+
+}
+
+index () {
+    let form = (document.getElementById(" ").value);
+    let NextPage = (document.getElementById("about.html").NextPage);
+    let confirm = (document.getElementById("num3").value);
+    
+}
+
+about () {
+    let NextPage = (document.getElementById("project.html").NextPage);
+    let BackPage = (document.getElementById("index.html").BackPage);
+    let 
+}
+
+project () {
+    
+}
