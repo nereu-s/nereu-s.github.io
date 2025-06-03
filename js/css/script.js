a6896a102250e96ef2115cc5a3c96881eb48e4ca
nereu-s <nereu.s@stu.janesville.k12.wi.us>
2025-06-03 14:35:20 -0500

    6/3

--- js/css/script.js
+++ js/css/script.js
@@ -1,21 +1,42 @@
 functions () {
     let Options = (document.getElementById("new").value);
-
+ 
 }
-
+ 
 index () {
     let form = (document.getElementById(" ").value);
     let NextPage = (document.getElementById("about.html").NextPage);
     let confirm = (document.getElementById("num3").value);
     
 }
-
+ 
 about () {
     let NextPage = (document.getElementById("project.html").NextPage);
     let BackPage = (document.getElementById("index.html").BackPage);
+}
+ 
+project () {
+    let NextPage = (document.getElementById("project.html").NextPage);
+    let BackPage = (document.getElementById("index.html").BackPage);
+}
+ 
+settings () {
+    let Options = (document.getElementById("projects.html").Options);
+}
+
+next () {
+    NextPage="new"
+    let NextPage= num1 * num3
+}
+
+back () {
+    let BackPage= (document.getElementById("index.html", "about.html", "projects.html").back);
+    if num1= (document.getElementById("back").value);
+}
+
+front () {
+    let FrontPage= (document.getElementById("about.html", "projects.html").front);
+    if num2= n1 + n4, let FrontPage= (document.getElementById("index.html").front);
     let 
 }
 
-project () {
-    
-}
